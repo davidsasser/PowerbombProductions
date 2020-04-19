af15182b2b6a32afbe3db390c74702b954c1d458
David Sasser <david.alan.sasser@gmail.com>
2020-04-18 20:56:01 -0500

    Added pinnned topics styling and formatting

--- environment/sql/topics.sql
+++ environment/sql/topics.sql
@@ -9,5 +9,6 @@ CREATE TABLE "topics"(
 	"subject" INT NOT NULL,
  	"message"  TEXT,
 	"created_on" TIMESTAMP NOT NULL,
+	"pinned" BOOLEAN DEFAULT FALSE,
  	"user_id" INT REFERENCES user_account (user_id)
 );
--- routes/forums/index.js
+++ routes/forums/index.js
@@ -4,7 +4,13 @@ const auth = require('../../helpers/auth');
 const fs = require('fs');
 
 forums.get("/wwe", (req,res) => {
-    var topics = {}
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`
+    }
     for(var i = 1; i < 10; i++) {
         topics[i] = {
             "replies": Math.floor(Math.random() * 90 + 10),
@@ -12,7 +18,7 @@ forums.get("/wwe", (req,res) => {
             "title": `This is title # ${i}.`
         }
     }
-    res.render('forum', {active: { forum: true }, topics: topics});
+    res.render('forum', {active: { forum: true }, topics: topics, pinned: pinned});
 });
 
 forums.get("/aew", (req,res) => {
