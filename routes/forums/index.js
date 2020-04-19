5267da586cabd2ef1f2592df93ff6266470b18d3
David Sasser <david.alan.sasser@gmail.com>
2020-04-18 22:24:28 -0500

    Added post request for deleting and pinning/unpinning topics

--- routes/forums/index.js
+++ routes/forums/index.js
@@ -9,13 +9,17 @@ forums.get("/wwe", (req,res) => {
     pinned[1] = {
         "replies": Math.floor(Math.random() * 90 + 10),
         "user": "test-user",
-        "title": `This is title is for the pinned tweet.`
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
     }
     for(var i = 1; i < 10; i++) {
         topics[i] = {
             "replies": Math.floor(Math.random() * 90 + 10),
             "user": "test-user",
-            "title": `This is title # ${i}.`
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
         }
     }
     res.render('forum', {active: { forum: true }, topics: topics, pinned: pinned});
@@ -29,16 +33,40 @@ forums.get("/njpw", auth.authenticationMiddleware(), (req,res) => {
 
 });
 
-forums.post("/general", auth.authenticationMiddleware(), (req, res) => {
+forums.get("/general", auth.authenticationMiddleware(), (req, res) => {
 
 });
 
-forums.post("/off-topic", auth.authenticationMiddleware(), (req, res) => {
+forums.get("/off-topic", auth.authenticationMiddleware(), (req, res) => {
 
 });
 
-forums.post("/trending", auth.authenticationMiddleware(), (req, res) => {
+forums.get("/trending", auth.authenticationMiddleware(), (req, res) => {
 
+});
+
+forums.post("/remove_topic/:topic_type/:id", auth.authenticationMiddleware(), (req, res) => {
+    var topic_id = req.params.id;
+    var topic_type = req.params.topic_type;
+    console.log(`Topic ID is ${topic_id} of topic type ${topic_type}`)
+
+    res.redirect(`/forums/${topic_type}`)
+});
+
+forums.post("/pin_topic/:topic_type/:id", auth.authenticationMiddleware(), (req, res) => {
+    var topic_id = req.params.id;
+    var topic_type = req.params.topic_type;
+    console.log(`Topic ID is ${topic_id} of topic type ${topic_type}`)
+
+    res.redirect(`/forums/${topic_type}`)
+});
+
+forums.post("/unpin_topic/:topic_type/:id", auth.authenticationMiddleware(), (req, res) => {
+    var topic_id = req.params.id;
+    var topic_type = req.params.topic_type;
+    console.log(`Topic ID is ${topic_id} of topic type ${topic_type}`)
+
+    res.redirect(`/forums/${topic_type}`)
 });
 
 module.exports = forums;
