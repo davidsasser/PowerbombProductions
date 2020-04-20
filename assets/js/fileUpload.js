170f56beb7a5c485ec681a94cdd899da69d677a6
David Sasser <david.alan.sasser@gmail.com>
2020-04-19 23:11:18 -0500

    Added forum post page and functionality

--- assets/js/fileUpload.js
+++ assets/js/fileUpload.js
@@ -7,3 +7,20 @@ $(document).ready(function(){
         $("label#fileNameLabel").text($('input#imageFile')[0].files[0].name);
     });
 });
+
+$(document).ready(function(){
+    $("input#forum-text-input").click(function () {
+        var topic_type = $("input[name='forum-input-val']").val();
+        window.location.href = `/forums/post_forum?topic_type=${topic_type}`
+    });
+});
+
+$(document).ready(function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    var queryString = urlParams.get('topic_type');
+    $("#forum-topic-select > option").each(function() {
+      if (this.value == queryString) {
+        this.selected = 'selected';
+      }
+    });
+  });
--- routes/forums/index.js
+++ routes/forums/index.js
@@ -4,6 +4,24 @@ const auth = require('../../helpers/auth');
 const fs = require('fs');
 
 forums.get("/wwe", (req,res) => {
+    // if(!topic_types.includes(topic_type)) {
+    //     res.status(404);
+
+    //     // respond with html page
+    //     if (req.accepts('html')) {
+    //         res.render('404', { url: req.url });
+    //         return;
+    //     }
+
+    //     // respond with json
+    //     if (req.accepts('json')) {
+    //         res.send({ error: 'Not found' });
+    //         return;
+    //     }
+
+    //     // default to plain-text. send()
+    //     res.type('txt').send('Not found');
+    // }
     var topics = {};
     var pinned = {};
     pinned[1] = {
@@ -22,27 +40,122 @@ forums.get("/wwe", (req,res) => {
             "topic_type": "wwe"
         }
     }
-    res.render('forum', {active: { forum: true }, topics: topics, pinned: pinned});
+    res.render('forum', {active: { forum: true, wwe: true }, topics: topics, pinned: pinned, topic_title: "WWE", topic_type: "wwe"});
 });
 
 forums.get("/aew", (req,res) => {
-
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
+    }
+    for(var i = 1; i < 10; i++) {
+        topics[i] = {
+            "replies": Math.floor(Math.random() * 90 + 10),
+            "user": "test-user",
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
+        }
+    }
+    res.render('forum', {active: { forum: true, aew: true }, topics: topics, pinned: pinned, topic_title: "AEW", topic_type: "aew"});
 });
 
-forums.get("/njpw", auth.authenticationMiddleware(), (req,res) => {
-
+forums.get("/njpw", (req,res) => {
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
+    }
+    for(var i = 1; i < 10; i++) {
+        topics[i] = {
+            "replies": Math.floor(Math.random() * 90 + 10),
+            "user": "test-user",
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
+        }
+    }
+    res.render('forum', {active: { forum: true, njpw: true }, topics: topics, pinned: pinned, topic_title: "NJPW", topic_type: "njpw"});
 });
 
-forums.get("/general", auth.authenticationMiddleware(), (req, res) => {
-
+forums.get("/general", (req,res) => {
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
+    }
+    for(var i = 1; i < 10; i++) {
+        topics[i] = {
+            "replies": Math.floor(Math.random() * 90 + 10),
+            "user": "test-user",
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
+        }
+    }
+    res.render('forum', {active: { forum: true, general: true }, topics: topics, pinned: pinned, topic_title: "General", topic_type: "general"});
 });
 
-forums.get("/off-topic", auth.authenticationMiddleware(), (req, res) => {
-
+forums.get("/off_topic", (req,res) => {
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
+    }
+    for(var i = 1; i < 10; i++) {
+        topics[i] = {
+            "replies": Math.floor(Math.random() * 90 + 10),
+            "user": "test-user",
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
+        }
+    }
+    res.render('forum', {active: { forum: true, off_topic: true }, topics: topics, pinned: pinned, topic_title: "Off Topic", topic_type: "off_topic"});
 });
 
-forums.get("/trending", auth.authenticationMiddleware(), (req, res) => {
+forums.get("/trending", (req,res) => {
+    var topics = {};
+    var pinned = {};
+    pinned[1] = {
+        "replies": Math.floor(Math.random() * 90 + 10),
+        "user": "test-user",
+        "title": `This is title is for the pinned tweet.`,
+        "topic_id": Math.floor(Math.random() * 10 + 1),
+        "topic_type": "wwe"
+    }
+    for(var i = 1; i < 10; i++) {
+        topics[i] = {
+            "replies": Math.floor(Math.random() * 90 + 10),
+            "user": "test-user",
+            "title": `This is title # ${i}.`,
+            "topic_id": Math.floor(Math.random() * 10 + 1),
+            "topic_type": "wwe"
+        }
+    }
+    res.render('forum', {active: { forum: true, trending: true }, topics: topics, pinned: pinned, topic_type: "Trending"});
+});
 
+forums.get("/post_forum", (req,res) => {
+    var topic_type = req.query.topic_type;
+    res.render('post_forum', {topic_type: topic_type})
 });
 
 forums.post("/remove_topic/:topic_type/:id", auth.authenticationMiddleware(), (req, res) => {
